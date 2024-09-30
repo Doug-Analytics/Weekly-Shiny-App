8d1285878977415829335e3a20c207881a195d7d
Doug-Analytics <128551642+Doug-Analytics@users.noreply.github.com>
2024-09-30 12:59:16 -0400

    Update Weekly Bar Graph Shiny App DataFrame.R

--- Weekly Bar Graph Shiny App DataFrame.R
+++ Weekly Bar Graph Shiny App DataFrame.R
@@ -12,6 +12,7 @@ source("ftn_functions.R")
 
 SEASON <- 2024
 key <- Sys.getenv("API_KEY")  # Use the API key from the environment variable
+#key <- "bd99fcb1-bc62-4655-822c-9eb02d09d420"
 
 
 ftn_charts <- ftn_game_data(type = "charts", year = SEASON)
@@ -24,87 +25,88 @@ req <- GET(url, add_headers(Authorization = key))
 ftn_games <- as.data.frame(fromJSON(rawToChar(req$content)))
 
 
-  
-  scores <- load_schedules(season = SEASON) %>%
-    select(week, home_team, home_score, away_team, away_score)
 
-  
-  teams <- load_teams() %>%
-    select(team_abbr, team_name, team_color, team_logo_espn)
+scores <- load_schedules(season = SEASON) %>%
+  select(week, home_team, home_score, away_team, away_score)
 
-  concept <- ftn_charts %>%
-      left_join(ftn_plays, by = c("gid", "pid"), relationship = "many-to-many") %>%
-      left_join(ftn_games, by = "gid", relationship = "many-to-many") %>%
-      filter(!is.na(concept), concept != "", concept != "Kneel", !is.na(off.x), !is.na(def.x)) %>%
-      mutate(off.x = case_when(
-        off.x == "CLV" ~ "CLE",
-        off.x == "BLT" ~ "BAL",
-        off.x == "ARZ" ~ "ARI",
-        off.x == "HST" ~ "HOU",
-        TRUE ~ off.x
-      )) %>%
-      mutate(def.x = case_when(
-        def.x == "CLV" ~ "CLE",
-        def.x == "BLT" ~ "BAL",
-        def.x == "ARZ" ~ "ARI",
-        def.x == "HST" ~ "HOU",
-        TRUE ~ def.x
-      )) %>%
-    mutate(concept = case_when(
-      concept == "Inside Zone" ~ "Inside Zone",  
-      concept == "Outside Zone" ~ "Outside Zone",  
-      concept == "Man/Duo" ~ "Man/Duo",  
-      concept == "Power" ~ "Power",  
-      concept == "Counter" ~ "Counter",              
-      concept == "Trap" ~ "Trap",  
-      concept == "Draw" ~ "Draw",  
-      concept == "Fullback Run" ~ "WR/FB Run",  
-      concept == "QB Sneak" ~ "QB Sneak",
-      concept == "Trick/WR Run" ~ "WR/FB Run",  
-      TRUE ~ "Other"
-    )) %>%
-    group_by(seas, week, off.x) %>%
-    mutate(snaps = n()) %>%
-    ungroup() %>%
-    group_by(seas, team = off.x, opp = def.x, week, concept) %>%
-    summarize(plays = n(), rate = plays/last(snaps), .groups = "drop") 
-  
- shell <- ftn_charts %>%
-      left_join(ftn_plays, by = c("gid", "pid"), relationship = "many-to-many") %>%
-      left_join(ftn_games, by = "gid", relationship = "many-to-many") %>%
-      filter(!is.na(shell), shell != "", !is.na(off.x), !is.na(def.x)) %>%
-      mutate(off.x = case_when(
-        off.x == "CLV" ~ "CLE",
-        off.x == "BLT" ~ "BAL",
-        off.x == "ARZ" ~ "ARI",
-        off.x == "HST" ~ "HOU",
-        TRUE ~ off.x
-      )) %>%
-      mutate(def.x = case_when(
-        def.x == "CLV" ~ "CLE",
-        def.x == "BLT" ~ "BAL",
-        def.x == "ARZ" ~ "ARI",
-        def.x == "HST" ~ "HOU",
-        TRUE ~ def.x
-      )) %>%
-    mutate(shell = case_when(
-      shell == 0 ~ "Cover 0",  
-      shell == 1 ~ "Cover 1",  
-      shell == 2 ~ "Cover 2",  
-      shell == 3 ~ "Cover 3",   
-      shell == 4 ~ "Cover 4",            
-      shell == 6 ~ "Cover 6",  
-      shell == 9 ~ "Cover 9",  
-      shell == "2M" ~ "Cover 2 Man",  
-      TRUE ~ "Other"
-    )) %>%
-    group_by(seas, week, off.x) %>%
-    mutate(snaps_shell = n()) %>%
-    ungroup() %>%
-    group_by(seas, team = def.x, opp = off.x, week, shell) %>%
-    summarize(plays_shell = n(), rate = plays_shell/last(snaps_shell), .groups = "drop") 
 
-data <- concept %>%
- left_join(shell, by = c("seas", "team", "opp", "week"))
+teams <- load_teams() %>%
+  select(team_abbr, team_name, team_color, team_logo_espn)
 
-  saveRDS(data, "Weekly_Bar_Graph_data.rds")
+concept <- ftn_charts %>%
+  left_join(ftn_plays, by = c("gid", "pid"), relationship = "many-to-many") %>%
+  left_join(ftn_games, by = "gid", relationship = "many-to-many") %>%
+  filter(!is.na(concept), concept != "", concept != "Kneel", !is.na(off.x), !is.na(def.x)) %>%
+  mutate(off.x = case_when(
+    off.x == "CLV" ~ "CLE",
+    off.x == "BLT" ~ "BAL",
+    off.x == "ARZ" ~ "ARI",
+    off.x == "HST" ~ "HOU",
+    TRUE ~ off.x
+  )) %>%
+  mutate(def.x = case_when(
+    def.x == "CLV" ~ "CLE",
+    def.x == "BLT" ~ "BAL",
+    def.x == "ARZ" ~ "ARI",
+    def.x == "HST" ~ "HOU",
+    TRUE ~ def.x
+  )) %>%
+  mutate(concept = case_when(
+    concept == "Inside Zone" ~ "Inside Zone",  
+    concept == "Outside Zone" ~ "Outside Zone",  
+    concept == "Man/Duo" ~ "Man/Duo",  
+    concept == "Power" ~ "Power",  
+    concept == "Counter" ~ "Counter",              
+    concept == "Trap" ~ "Trap",  
+    concept == "Draw" ~ "Draw",  
+    concept == "Fullback Run" ~ "WR/FB Run",  
+    concept == "QB Sneak" ~ "QB Sneak",
+    concept == "Trick/WR Run" ~ "WR/FB Run",  
+    TRUE ~ "Other"
+  )) %>%
+  group_by(seas, week, off.x) %>%
+  mutate(snaps = n()) %>%
+  ungroup() %>%
+  group_by(seas, team = off.x, opp = def.x, week, category = concept) %>%
+  summarize(plays = n(),
+            rate = plays/last(snaps), .groups = "drop") 
+
+shell <- ftn_charts %>%
+  left_join(ftn_plays, by = c("gid", "pid"), relationship = "many-to-many") %>%
+  left_join(ftn_games, by = "gid", relationship = "many-to-many") %>%
+  filter(!is.na(shell), shell != "", !is.na(off.x), !is.na(def.x)) %>%
+  mutate(off.x = case_when(
+    off.x == "CLV" ~ "CLE",
+    off.x == "BLT" ~ "BAL",
+    off.x == "ARZ" ~ "ARI",
+    off.x == "HST" ~ "HOU",
+    TRUE ~ off.x
+  )) %>%
+  mutate(def.x = case_when(
+    def.x == "CLV" ~ "CLE",
+    def.x == "BLT" ~ "BAL",
+    def.x == "ARZ" ~ "ARI",
+    def.x == "HST" ~ "HOU",
+    TRUE ~ def.x
+  )) %>%
+  mutate(shell = case_when(
+    shell == 0 ~ "Cover 0",  
+    shell == 1 ~ "Cover 1",  
+    shell == 2 ~ "Cover 2",  
+    shell == 3 ~ "Cover 3",   
+    shell == 4 ~ "Cover 4",            
+    shell == 6 ~ "Cover 6",  
+    shell == 9 ~ "Cover 9",  
+    shell == "2M" ~ "Cover 2 Man",  
+    TRUE ~ "Other"
+  )) %>%
+  group_by(seas, week, off.x) %>%
+  mutate(snaps_shell = n()) %>%
+  ungroup() %>%
+  group_by(seas, team = def.x, opp = off.x, week, category = shell) %>%
+  summarize(plays = n(),
+            rate = plays/last(snaps_shell), .groups = "drop") 
+
+saveRDS(concept, "Weekly_Bar_Graph_data_concept.rds")
+
+saveRDS(shell, "Weekly_Bar_Graph_data_shell.rds")
